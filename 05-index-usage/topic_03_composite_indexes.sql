3c87f0818b6258eaecd634b1da7521e9329ec45c
dezlymacauley <dezlymacauley@proton.me>
2026-03-10 12:24:34 +0200

    Updated knowledge on composite indexes

--- 05-index-usage/topic_03_composite_indexes.sql
+++ 05-index-usage/topic_03_composite_indexes.sql
@@ -52,9 +52,71 @@ PRAGMA index_info("composite_index_fname_lname_bday");
 
 -------------------------------------------------------------------------------
 
+-- SECTION: Why the order of the columns matters when using a composite index
+
+-- When the composite index was declared,
+-- the order was: first_name, last_name, birthday
+
+-- CREATE INDEX composite_index_fname_lname_bday 
+-- on users (first_name, last_name, birthday);
+
+-------------------------------------------------------------------------------
+
+-- First I set the mode to vertical just to make the output 
+-- of the command easier.
+
+.mode vertical;
+
+-------------------------------------------------------------------------------
+
+-- Example 1
 EXPLAIN QUERY PLAN
-SELECT * FROM users WHERE last_name = 'Dezly';
+SELECT * FROM users WHERE first_name = 'Dezly';
 
+/*
+    ***************************[ 1. row ]***************************
+    id      | 3
+    parent  | 0
+    notused | 62
+    detail  | SEARCH users USING INDEX composite_index_fname_lname_bday (first_name=?)
+*/
 
+-- As you can see, the composite index will be used:
+
+-------------------------------------------------------------------------------
+
+-- Example 2
+EXPLAIN QUERY PLAN
+SELECT * FROM users 
+WHERE last_name = 'Macauley';
+
+/*
+    ***************************[ 1. row ]***************************
+    id      | 2
+    parent  | 0
+    notused | 216
+    detail  | SCAN users
+*/
+
+-- As you can see, the composite index will NOT be used
+-- This is because a query must use the `first_name` column first.
+
+-------------------------------------------------------------------------------
+
+-- Example 3
+EXPLAIN QUERY PLAN
+SELECT * FROM users 
+WHERE first_name = 'Dezly' AND last_name = 'Macauley';
+
+/*
+    ***************************[ 1. row ]***************************
+    id      | 3
+    parent  | 0
+    notused | 61
+    detail  | SEARCH users USING INDEX composite_index_fname_lname_bday (first_name=? AND last_name=?)
+*/
+
+-- As you can see, the composite index WILL be be used in this case,
+-- because the query uses the indexes in the the correct order.
 
 -------------------------------------------------------------------------------
